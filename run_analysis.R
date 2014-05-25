098242599396e0f154bfb1dde7851f66077b75ac
Patricia Coates <patriciajcrain@gmail.com>
2014-05-25 09:31:33 -0300

    tweaks to Script

--- run_analysis.R
+++ run_analysis.R
@@ -4,12 +4,13 @@ Analysis <- function(){
   features <- read.table("features.txt", header=F)
   features <- features[grepl("-mean\\(\\)|-std\\(\\)", features$V2),]
   features$V2 <- gsub("\\(\\)","",features$V2)
+  features$V2 <- gsub("\\-",".",features$V2)
   
   ## Read in the activity labels to be used later
   labels <- read.table("activity_labels.txt", header=F, col.names=c("Activity","Activity Label")) 
   
-  message("cleaning the test data...")
   ## Cleaning the test data
+  message("cleaning the test data...")
   test <- readLines("test/X_test.txt")
   test <- gsub("  ", " ", test)
   test <- gsub("^ ", "", test)
@@ -21,38 +22,43 @@ Analysis <- function(){
   test <- data.frame(test)
   names(test) <- as.character(features$V2)
   
+  ## Cleaning the train data
+  message("cleaning the train data...")
+  train <- readLines("train/X_train.txt")
+  train <- gsub("  ", " ", train)
+  train <- gsub("^ ", "", train)
+  writeLines(train,"train.txt")
+  
+  ## Read the train data back in and convert to data frame with meaningful feature labels
+  train <- fread("train.txt", header=F, select=features$V1)
+  train <- data.frame(train)
+  names(train) <- as.character(features$V2)
+  
+  message("adding subject and activity features to test and train...")
+  
   ## Add subject and activity columns to test data
   require(plyr)
   test$Subject <- readLines("test/subject_test.txt")
   test$Activity <- readLines("test/y_test.txt")
   test <- join(test,labels, by= "Activity")
-  
-  message("cleaning the train data...")
-  ## Cleaning the train data
-  train <- readLines("train/X_train.txt")
-  train <- gsub("  ", " ", train)
-  train <- gsub("^ ", "", train)
-  writeLines(train,"train.txt")
-    
-  ## Read the train data back in and convert to data frame with meaningful feature labels
-  train <- fread("train.txt", header=F, select=features$V1)
-  train <- data.frame(train)
-  names(train) <- as.character(features$V2)
-    
+      
   ## Add subject and activity columns to train data
   train$Subject <- readLines("train/subject_train.txt")
   train$Activity <- readLines("train/y_train.txt")
   train <- join(train,labels, by= "Activity")
   
   message("combining the test and train data together...")
+  
   ## Rbind test and train data sets together
   all <- rbind(test,train)
+  
   ## Find the average of each variable for each activity and each subject, remove NA columns
   message("computing the average for each variable by activity and subject...")
   final <- suppressWarnings(aggregate(all, by=list(Activity=all$Activity.Label, Subject=all$Subject), mean, length.warning=F))
   final <- final[,-c(69:71)]
-  message("writing out the tidy data set to 'tidydata.txt'")
-  write.csv(final, "tidydata.txt")
+  names(final)[3:68] <- paste0(names(final)[3:68],"-Avg")
+  message("writing out the tidy data set to 'HARdata_tidy.txt'")
+  write.csv(final, "HARdata_tidy.txt")
 }
 
 
